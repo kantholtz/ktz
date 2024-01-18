18c3ae617328c2bfcbe60e57c196c30c63dce355
Felix Hamann <felix@hamann.xyz>
2024-01-18 11:49:13 +0100

    updated docs

--- docs-src/source/conf.py
+++ docs-src/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Felix Hamann"
 author = "Felix Hamann"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.0"
+release = "0.4.0-pre0"
 
 
 # -- General configuration ---------------------------------------------------
